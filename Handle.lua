6643343c46aca3b01b19b444ebda762c33a3036c
zaCade <zaCade@hotmail.com>
2019-12-13 18:50:49 +0100

    Clean up code, add more frames.
    Cleaned up the code, and added frames i got from https://github.com/Suraf/BlizzMove, and fixed some issues like the code for the objective tracker.

--- Handle.lua
+++ Handle.lua
@@ -1,4 +1,4 @@
-﻿-- BlizzMmove, move the blizzard frames by yess
+-- BlizzMmove, move the blizzard frames by yess
 if not _G.BlizzMove then BlizzMove = {} end
 local BlizzMove = BlizzMove
 
@@ -9,40 +9,18 @@ function BlizzMove:CreateOwnHandleFrame(frame, width, height, offX, offY, name)
 	handle:SetParent(frame)
 	handle:EnableMouse(true)
 	handle:SetMovable(true)
-	handle:SetPoint("TOPLEFT", frame ,"TOPLEFT", offX, offY)
+	handle:SetPoint("TOPLEFT", frame, "TOPLEFT", offX, offY)
 	--[[
 	handle:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background",
-	                                        edgeFile = nil,
-	                                       tile = true, tileSize = 16, edgeSize = 16,
-	                                        insets = { left = 0, right = 0, top = 0, bottom = 0 }})
+						tile = true,
+						tileSize = 16,
+						edgeSize = 16,
+						insets = { left = 0, right = 0, top = 0, bottom = 0 }})
 	handle:SetBackdropColor(1,0,0,0.5)
+	handle:SetFrameStrata("MEDIUM")
 	--]]
-	--handle:SetFrameStrata("MEDIUM")
+
 	return handle
-end
-
-local function OnDragStart(self)
-	local frameToMove = self.frameToMove
-	if frameToMove:IsMovable() then
-    frameToMove:StartMoving()
-    frameToMove.isMoving = true
-  end
-end
-
-local function OnDragStop(self)
-	local frameToMove = self.frameToMove
-	frameToMove:StopMovingOrSizing()
-	frameToMove.isMoving = false
-end
-
-
-local function OnMouseWheel(self, vector, ...)
-	if not IsControlKeyDown() then return end
-	local scale = self.frameToMove:GetScale() or 1
-	scale = scale + .1 * vector
-	if scale > 1.5  then scale = 1.5
-	elseif scale < .5 then scale = .5 end
-	self.frameToMove:SetScale(scale)
 end
 
 function BlizzMove:CreateQuestTrackerHandle()
@@ -55,23 +33,51 @@ function BlizzMove:CreateQuestTrackerHandle()
 	ObjectiveTrackerFrame.BlocksFrame.QuestHeader:EnableMouse(true)
 	ObjectiveTrackerFrame.BlocksFrame.AchievementHeader:EnableMouse(true)
 	ObjectiveTrackerFrame.BlocksFrame.ScenarioHeader:EnableMouse(true)
+
 	return handle
 end
 
+local function OnDragStart(self)
+	if self.frameToMove:IsMovable() then
+		self.frameToMove:StartMoving()
+		self.frameToMove.isMoving = true
+	end
+end
+
+local function OnDragStop(self)
+	self.frameToMove:StopMovingOrSizing()
+	self.frameToMove.isMoving = false
+end
+
+local function OnMouseWheel(self, vector, ...)
+	if not IsControlKeyDown() then return end
+
+	local scale = self.frameToMove:GetScale() or 1
+
+	scale = scale + .1 * vector
+
+	if scale > 1.5 then scale = 1.5 end
+	if scale < 0.5 then scale = 0.5 end
+
+	self.frameToMove:SetScale(scale)
+end
+
 function BlizzMove:SetMoveHandle(frameToMove, handle)
-	if not frameToMove then
-		print("Expected frame got nil.")
+	if not frameToMove or not frameToMove.EnableMouse then
+		print("Expected frame got nil, or has mouse disabled.")
 		return
 	end
-	if not handle then handle = frameToMove end
-	handle.frameToMove = frameToMove
 
-	if not frameToMove.EnableMouse then return end
 	frameToMove:SetMovable(true)
+
+	if not handle then handle = frameToMove end
+
+	handle.frameToMove = frameToMove
 	handle:RegisterForDrag("LeftButton");
 
-	handle:SetScript("OnDragStart", OnDragStart)
-	handle:SetScript("OnDragStop", OnDragStop)
+	handle:HookScript("OnDragStart", OnDragStart)
+	handle:HookScript("OnDragStop", OnDragStop)
+	handle:HookScript("OnMouseWheel", OnMouseWheel)
+
 	handle:EnableMouseWheel(true)
-	handle:HookScript("OnMouseWheel",OnMouseWheel)
 end
