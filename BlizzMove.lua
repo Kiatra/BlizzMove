ec6012dd11a3c12b7099ae5624defe4614d8919d
zaCade <zaCade@hotmail.com>
2019-12-24 17:09:51 +0100

    Swap some frames around, refractor code and add can move functionality

--- BlizzMove.lua
+++ BlizzMove.lua
@@ -3,11 +3,12 @@
 -- Macro to get the parent of a frame under the mouse cursor:
 --/run f = GetMouseFocus(); if f then DEFAULT_CHAT_FRAME:AddMessage(f:GetParent():GetParent():GetName()) end
 --/run f = GetMouseFocus(); while f do DEFAULT_CHAT_FRAME:AddMessage(f:GetName()); f = f:GetParent() end
-local BlizzMove = _G.BlizzMove
+_G.BlizzMove = _G.BlizzMove or {}
 
 movableFrames = {
 	AddonList,
 	BankFrame,
+	CraftFrame,
 	DestinyFrame,
 	DressUpFrame,
 	ExtraActionBarFrame,
@@ -23,40 +24,38 @@ movableFrames = {
 	MerchantFrame,
 	PVEFrame,
 	QuestFrame,
+	QuestLogFrame,
 	QuestLogPopupDetailFrame,
 	RaidBrowserFrame,
+	SpellBookFrame,
 	SplashFrame,
 	SUFWrapperFrame,
 	TabardFrame,
-	TalkingHeadFrame,
 	TradeFrame,
 	VideoOptionsFrame,
-	WorldMapFrame
 }
 
 local tocversion = select(4, GetBuildInfo())
-if tocversion < 20000 then
-	table.insert(movableFrames, CraftFrame)
-end
-
-if tocversion < 80200 then
-	table.insert(movableFrames, QuestLogFrame)
+if tocversion > 20000 then
+	table.insert(movableFrames, WorldMapFrame)
 end
 
 movableFramesWithHandle = {
-	["CharacterFrame"] =  { PaperDollFrame, ReputationFrame, TokenFrame, PetPaperDollFrameCompanionFrame },
-	["ColorPickerFrame"] = { BlizzMove:CreateOwnHandleFrame(ColorPickerFrame, 132, 32, 117, 8, "ColorPickerFrame") },
+	["CharacterFrame"] =  { PaperDollFrame, PetPaperDollFrame, CompanionFrame, ReputationFrame, SkillFrame, HonorFrame, TokenFrame },
+	["ColorPickerFrame"] = { BlizzMove:CreateHandleFromPoints(ColorPickerFrame, ColorPickerFrame.Border.TopEdge) },
 	["MailFrame"] = { SendMailFrame },
-	["SpellBookFrame"] = { BlizzMove:CreateOwnHandleFrame(SpellBookFrame, 525, 20, 0, 0, "SpellBookFrame") },
-	["WorldMapFrame"] = { WorldMapTitleButton },
---	["ObjectiveTrackerFrame"] = { BlizzMove:CreateQuestTrackerHandle(), ObjectiveTrackerFrame.BlocksFrame.QuestHeader, ObjectiveTrackerFrame.BlocksFrame.AchievementHeader, ObjectiveTrackerFrame.BlocksFrame.ScenarioHeader },
+	["ObjectiveTrackerFrame"] = { BlizzMove:CreateHandleFromPoints(ObjectiveTrackerFrame, ObjectiveTrackerFrame.BlocksFrame) },
+}
+
+movableFramesCanMove = {
+	["ObjectiveTrackerFrame"] = function() return IsAltKeyDown() and IsShiftKeyDown() end,
 }
 
 movableFramesLoadOnDemand = {
 	["Blizzard_AchievementUI"] = function() BlizzMove:SetMoveHandle(AchievementFrame, AchievementFrameHeader) end,
 	["Blizzard_AlliedRacesUI"] = function() BlizzMove:SetMoveHandle(AlliedRacesFrame) end,
 	["Blizzard_ArchaeologyUI"] = function() BlizzMove:SetMoveHandle(ArchaeologyFrame) end,
-	["Blizzard_ArtifactUI"] = function()  BlizzMove:SetMoveHandle(ArtifactFrame); BlizzMove:SetMoveHandle(ArtifactRelicForgeFrame) end,
+	["Blizzard_ArtifactUI"] = function() BlizzMove:SetMoveHandle(ArtifactFrame); BlizzMove:SetMoveHandle(ArtifactRelicForgeFrame) end,
 	["Blizzard_AuctionUI"] = function() BlizzMove:SetMoveHandle(AuctionFrame) end,
 	["Blizzard_AzeriteEssenceUI"] = function() BlizzMove:SetMoveHandle(AzeriteEssenceUI) end,
 	["Blizzard_AzeriteRespecUI"] = function() BlizzMove:SetMoveHandle(AzeriteRespecFrame) end,
@@ -68,11 +67,11 @@ movableFramesLoadOnDemand = {
 	["Blizzard_Collections"] = function() BlizzMove:SetMoveHandle(CollectionsJournal); BlizzMove:SetMoveHandle(WardrobeFrame) end,
 	["Blizzard_Communities"] = function() BlizzMove:SetMoveHandle(CommunitiesFrame) end,
 	["Blizzard_DeathRecap"] = function() BlizzMove:SetMoveHandle(DeathRecapFrame) end,
-	["Blizzard_EncounterJournal"] = function() BlizzMove:SetMoveHandle(EncounterJournal, BlizzMove:CreateOwnHandleFrame(EncounterJournal, 775, 20, 0, 0, "EncounterJournal")) end,
+	["Blizzard_EncounterJournal"] = function() BlizzMove:SetMoveHandle(EncounterJournal) end,
 	["Blizzard_FlightMap"] = function() BlizzMove:SetMoveHandle(FlightMapFrame) end,
 	["Blizzard_GarrisonUI"] = function() BlizzMove:SetMoveHandle(GarrisonLandingPage); BlizzMove:SetMoveHandle(GarrisonMissionFrame); BlizzMove:SetMoveHandle(GarrisonBuildingFrame); BlizzMove:SetMoveHandle(GarrisonShipyardFrame); BlizzMove:SetMoveHandle(GarrisonCapacitiveDisplayFrame); BlizzMove:SetMoveHandle(OrderHallMissionFrame); BlizzMove:SetMoveHandle(BFAMissionFrame); end,
 	["Blizzard_GuildBankUI"] = function() BlizzMove:SetMoveHandle(GuildBankFrame) end,
-	["Blizzard_GuildUI"] = function() BlizzMove:SetMoveHandle(GuildFrame, GuildFrame.TitleMouseover) end,
+	["Blizzard_GuildUI"] = function() BlizzMove:SetMoveHandle(GuildFrame) end,
 	["Blizzard_InspectUI"] = function() BlizzMove:SetMoveHandle(InspectFrame) end,
 	["Blizzard_IslandsQueueUI"] = function() BlizzMove:SetMoveHandle(IslandsQueueFrame) end,
 	["Blizzard_ItemAlterationUI"] = function() BlizzMove:SetMoveHandle(TransmogrifyFrame) end,
@@ -82,10 +81,11 @@ movableFramesLoadOnDemand = {
 	["Blizzard_MacroUI"] = function() BlizzMove:SetMoveHandle(MacroFrame) end,
 	["Blizzard_ObliterumUI"] = function() BlizzMove:SetMoveHandle(ObliterumForgeFrame) end,
 	["Blizzard_PartyPoseUI"] = function() BlizzMove:SetMoveHandle(PartyPoseFrame) end,
-	["Blizzard_ReforgingUI"] = function() BlizzMove:SetMoveHandle(ReforgingFrame, ReforgingFrameInvisibleButton) end,
+	["Blizzard_PVPUI"] = function() BlizzMove:SetMoveHandle(PVPMatchScoreboard) end,
+	["Blizzard_ReforgingUI"] = function() BlizzMove:SetMoveHandle(ReforgingFrame) end,
 	["Blizzard_ScrappingMachineUI"] = function() BlizzMove:SetMoveHandle(ScrappingMachineFrame) end,
 --	["Blizzard_StoreUI"] = function() BlizzMove:SetMoveHandle(StoreFrame) end, -- Forbidden access.
-	["Blizzard_TalentUI"] = function() BlizzMove:SetMoveHandle(PlayerTalentFrame); if tocversion < 20000 then BlizzMove:SetMoveHandle(TalentFrame) end end,
+	["Blizzard_TalentUI"] = function() if tocversion > 20000 then BlizzMove:SetMoveHandle(PlayerTalentFrame) else BlizzMove:SetMoveHandle(TalentFrame) end end,
 	["Blizzard_TalkingHeadUI"] = function() BlizzMove:SetMoveHandle(TalkingHeadFrame) end,
 	["Blizzard_TradeSkillUI"] = function() BlizzMove:SetMoveHandle(TradeSkillFrame) end,
 	["Blizzard_TrainerUI"] = function() BlizzMove:SetMoveHandle(ClassTrainerFrame) end,
@@ -100,7 +100,7 @@ function movableFramesLoadOnDemand:BlizzMove()
 	end
 
 	for frame, handles in pairs(movableFramesWithHandle) do
-		for index, handle in pairs(handles) do
+		for _, handle in pairs(handles) do
 			BlizzMove:SetMoveHandle(_G[frame], handle)
 		end
 	end
