e043e25a765e7e070bd649dc34198d94ec12564e
Mark W <numedain@gmail.com>
2024-08-25 13:29:09 +0200

    Fix a bug with Hero talent popup, attempt number 4 -.- (#132)

--- BlizzMove.lua
+++ BlizzMove.lua
@@ -1333,7 +1333,11 @@ do
                 frame:SetMovable(backup);
             end
             startStopMoving(_G.HeroTalentsSelectionDialog);
-            PlayerSpellsFrame:HookScript('OnShow', function(frame)
+            _G.PlayerSpellsFrame:HookScript('OnShow', function(frame)
+                startStopMoving(frame);
+                RunNextFrame(function() startStopMoving(frame); end);
+            end);
+            _G.HeroTalentsSelectionDialog:HookScript('OnShow', function(frame)
                 startStopMoving(frame);
                 RunNextFrame(function() startStopMoving(frame); end);
             end);
