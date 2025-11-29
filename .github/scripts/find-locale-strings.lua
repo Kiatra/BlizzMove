9e3e59f9b46cefbb07fefed78b36d91ebb4c946e
Mark W <numedain@gmail.com>
2025-11-29 15:35:55 +0100

    Improved the script to automate localization

--- .github/scripts/find-locale-strings.lua
+++ .github/scripts/find-locale-strings.lua
@@ -1,99 +1,83 @@
--- Adapted from https://github.com/Nevcairiel/Bartender4/blob/master/locale/find-locale-strings.lua
-local TOC_FILES = {}
+-- Inspired by https://github.com/Nevcairiel/Bartender4/blob/master/locale/find-locale-strings.lua
+local LUA_FILES = {}
 -- Automatically find the TOC in the given path, set to false to disable
 local outputFilename = arg and arg[1] or "exported-locale-strings.lua"
-local AUTO_FIND_TOC = { "./" }
+local PATHS = { "./" }
 if arg and arg[2] then
     local i = 2
     while arg[i] do
-        AUTO_FIND_TOC[i-1] = arg[i]
+        PATHS[i - 1] = arg[i]
         i = i + 1
     end
 end
 
 -- Patterns that should not be scrapped, case-insensitive
 -- Anything between the no-lib-strip is automatically ignored
-local FILE_BLACKLIST = {"^locale", "^libs"}
+local FILE_BLACKLIST = { "/locale/", "/libs/", "/types.lua" }
 
 
 -- No more modifying!
 local OS_TYPE = os.getenv("HOME") and "linux" or "windows"
+if OS_TYPE == "windows" then
+    error("Only Linux is supported at this time.")
+end
 
--- Find the TOC now
-for _, path in ipairs(AUTO_FIND_TOC) do
-    local pipe = OS_TYPE == "windows" and io.popen(string.format("dir /B \"%s\"", path)) or io.popen(string.format("ls -1 \"%s\"", path))
-    if( type(pipe) == "userdata" ) then
+for _, path in ipairs(PATHS) do
+    local pipe = io.popen(string.format("find \"%s\" -type f -name '*.lua'", path))
+    if type(pipe) == "userdata" then
         for file in pipe:lines() do
-            if( string.match(file, "(.+)%.toc") ) then
-                table.insert(TOC_FILES, { path = path, toc = path .. file })
-                break
+            if string.match(file, "(.+)%.lua") then
+                table.insert(LUA_FILES, file)
             end
         end
 
         pipe:close()
-        if( not TOC_FILES ) then print("Failed to auto detect toc file.") end
     else
-        print("Failed to auto find toc, cannot run dir /B or ls -1")
+        error("Failed to auto find lua files")
     end
 end
 
-if( not next(TOC_FILES) ) then
-    return
+if not next(LUA_FILES) then
+    error("Failed to auto detect toc file.")
 end
 
-local ignore
 local localizedKeys = {}
-for _, tocFile in ipairs(TOC_FILES) do
-    local toc, path = tocFile.toc, tocFile.path
-    print(string.format("Using TOC file %s", toc))
-    print("")
-    -- Parse through the TOC file so we know what to scan
-    for line in io.lines(toc) do
-        line = path .. string.gsub(line, "\r", "")
-
-        if( string.match(line, "#@no%-lib%-strip@") ) then
-            ignore = true
-        elseif( string.match(line, "#@end%-no%-lib%-strip@") ) then
-            ignore = nil
+for _, file in ipairs(LUA_FILES) do
+    if string.match(file, "%.lua") and not string.match(file, "^%s*#") then
+        -- Make sure it's a valid file
+        local blacklist
+        for _, check in pairs(FILE_BLACKLIST) do
+            if string.match(string.lower(file), check) then
+                blacklist = true
+                break
+            end
         end
 
-        if( not ignore and string.match(line, "%.lua") and not string.match(line, "^%s*#")) then
-            -- Make sure it's a valid file
-            local blacklist
-            for _, check in pairs(FILE_BLACKLIST) do
-                if( string.match(string.lower(line), check) ) then
-                    blacklist = true
-                    break
-                end
+        -- File checks out, scrap everything
+        if not blacklist then
+            -- Fix slashes
+            if OS_TYPE == "linux" then
+                file = string.gsub(file, "\\", "/")
             end
 
-            -- File checks out, scrap everything
-            if( not blacklist ) then
-                -- Fix slashes
-                if( OS_TYPE == "linux" ) then
-                    line = string.gsub(line, "\\", "/")
-                end
+            local keys = 0
+            local contents = io.open(file):read("*all")
 
-                local keys = 0
-                local contents = io.open(line):read("*all")
-
-                for match in string.gmatch(contents, "L%[\"(.-)\"]") do
-                    if( not localizedKeys[match] ) then keys = keys + 1 end
-                    localizedKeys[match] = true
-                end
-                for match in string.gmatch(contents, "L%['(.-)']") do
-                    -- convert format from single to double quotes
-                    match = string.gsub(match, "\\'", "'")
-                    match = string.gsub(match, "\"", "\\\"")
-                    if( not localizedKeys[match] ) then keys = keys + 1 end
-                    localizedKeys[match] = true
-                end
-
-                print(string.format("%s (%d keys)", line, keys))
+            for match in string.gmatch(contents, "L%[\"(.-)\"]") do
+                if not localizedKeys[match] then keys = keys + 1 end
+                localizedKeys[match] = true
             end
+            for match in string.gmatch(contents, "L%['(.-)']") do
+                -- convert format from single to double quotes
+                match = string.gsub(match, "\\'", "'")
+                match = string.gsub(match, "\"", "\\\"")
+                if not localizedKeys[match] then keys = keys + 1 end
+                localizedKeys[match] = true
+            end
+
+            print(string.format("%s (%d keys)", file, keys))
         end
     end
-    print("")
 end
 
 -- Compile all of the localization we found into string form
@@ -104,7 +88,7 @@ for key in pairs(localizedKeys) do
     totalLocalizedKeys = totalLocalizedKeys + 1
 end
 
-if( totalLocalizedKeys == 0 ) then
+if totalLocalizedKeys == 0 then
     print("Warning, failed to find any localizations, perhaps you messed up a configuration variable?")
     return
 end
--- .github/scripts/update_translation.sh
+++ .github/scripts/update_translation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Adapted from https://github.com/Nevcairiel/Bartender4/blob/master/locale/wowace-locale-import.sh
+# Inspired by https://github.com/Nevcairiel/Bartender4/blob/master/locale/wowace-locale-import.sh
 
 cf_token=
 
